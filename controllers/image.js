f70237c77781d259b0d929f940565034a3920775
OnrHng <hangul07@hacettepe.edu.tr>
2020-02-26 15:24:25 +0100

    fixing for security

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require ('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '6b3aeb6efa814671beaaf12c6aa52c11'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+	 .predict(
+       Clarifai.FACE_DETECT_MODEL, req.body.input)
+	 .then(data => res.json(data))
+	 .catch(err => res.status(400).json('unable access with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users')
+	  .where('id', '=', id)
+	  .increment('entries', 1)
+	  .returning('entries' )
+	  .then(entries	=> {
+	  	res.json(entries[0]);
+	  })
+	  .catch(err => res.status(400).json('unable get entries'))
+}
+
+module.exports = {
+	handleImage : handleImage,
+	handleApiCall : handleApiCall
+}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require ('cors');
 const knex = require ('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+const image = require('./controllers/image');
 
 const db = require('knex')({
   client: 'pg',
@@ -27,7 +28,8 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt) })
-
+app.put ('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post ('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 app.get('/profile/:id', (req,res) =>{
 	const { id } = req.params;
 	db.select('*').from('users').where({
@@ -41,18 +43,6 @@ app.get('/profile/:id', (req,res) =>{
 			}
 	})
 		.catch(err => res.status(400).json('ERRROOORR'))
-})
-
-app.put ('/image', (req, res) => {
-	const { id } = req.body;
-	db('users')
-	  .where('id', '=', id)
-	  .increment('entries', 1)
-	  .returning('entries' )
-	  .then(entries	=> {
-	  	res.json(entries[0]);
-	  })
-	  .catch(err => res.status(400).json('unable get entries'))	
 })
 
 app.listen(3001, ()=> {
